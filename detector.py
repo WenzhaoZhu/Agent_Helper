2e3c7c9e2277037bdfbcca5ea5c8da8e845e19f6
WenzhaoZhu <38002859+WenzhaoZhu@users.noreply.github.com>
2024-08-19 17:51:52 -0600

    Add files via upload

--- detector.py
+++ detector.py
@@ -1,44 +1,75 @@
 import re
 import os
+import opencc
 import string
 from nltk import ngrams
 from collections import Counter
 
-RELA_PATH = "." # Path of the file
-FILE_NAME = "response.txt" # Name of the file
+RELA_PATH = "."  # Path of the file
+FILE_NAME = "response.txt"  # Name of the file
+
 
 def read_file(path):
     """
     read the file into a string
     """
-    with open(path, "r", encoding='utf-8') as f:
+    with open(path, "r", encoding="utf-8") as f:
         input_chinese = f.read()
     return input_chinese
 
+
 def NTLDetector(text):
+    """
+    Detect non-Chinese characters, specifically English letters and punctuation.
+    """
     count = 0
     for chara in text:
-        if 'A' <= chara <= 'Z' or 'a' <= chara <= 'z':
+        if "A" <= chara <= "Z" or "a" <= chara <= "z":
             print("English letter --", chara, "-- detected!")
             count = count + 1
         if chara in ":;,.?!'\"()":
             print("English punctuation --", chara, "-- detected!")
             count = count + 1
-    print("The amount of English letters and English punc is: ", count)
+    if count != 0:
+        print("The amount of English letters and English punc is: ", count)
+
 
 def other_non_cn(text):
+    """
+    Detect other non-Chinese characters, such as numbers and emojis.
+    """
     count = 0
     for chara in text:
-        if not 'A' <= chara <= 'Z' and not 'a' <= chara <= 'z' and chara not in ":;,.?!'\"()":
-            if not '\u4e00' <= chara <= '\u9fff':
+        if (
+            not "A" <= chara <= "Z"
+            and not "a" <= chara <= "z"
+            and chara not in ":;,.?!'\"()"
+        ):
+            if not "\u4e00" <= chara <= "\u9fff":
                 print("Non Chinese character --", chara, "-- detected!")
                 count = count + 1
-    print("The amount of other non-Chinese characters is: ", count)
-    
+    if count != 0:
+        print("The amount of other non-Chinese characters is: ", count)
+
+
+def traditional_chinese_detect(text):
+    """
+    Detect Traditional Chinese.
+    """
+    converter = opencc.OpenCC("t2s.json")
+    converted_chinese = converter.convert(text)
+    if text != converted_chinese:
+        print("WARNING -- Traditional Chinese -- Detected! Check if they are legal:")
+        trad_cn = []
+        for idx, _ in enumerate(text):
+            if text[idx] != converted_chinese[idx]:
+                trad_cn.append(text[idx])
+        print(trad_cn)
+
+
 def clean_text(text):
     """
     Clean up the punctuations and whitespaces that may appear in the text
-
     """
     # print("Length before eliminate white changelines: ", len(text))
 
@@ -53,7 +84,9 @@ def clean_text(text):
     # print("Text: ", text)
     print("Characters (without whitespaces) in total: ", len(text))
     if len_before_eli != len(text):
-        print("There is/are -- space(s) -- in the response, check if it/they is/are legal!")
+        print(
+            "There is/are -- space(s) -- in the response, check if it/they is/are legal!"
+        )
         print("Number of spaces: ", len_before_eli - len(text))
 
     # Eliminate HTML lables and entities
@@ -83,23 +116,25 @@ def clean_text(text):
             "）": "",
             "《": "",
             "》": "",
-
         }
     )
-    translator.update(str.maketrans("", "", string.punctuation)) # type: ignore
+    translator.update(str.maketrans("", "", string.punctuation))  # type: ignore
 
     len_before_eli_punc = len(text)
     text = text.translate(translator)
     num_of_punc = len_before_eli_punc - len(text)
 
     other_non_cn(text)
+    traditional_chinese_detect(text)
 
     count = 0
-    for a in text:
-        if '\u4e00' <= a <= '\u9fff':
+    for a in text:  # Chinese Characters range in between 4e00 and 9fff
+        if "\u4e00" <= a <= "\u9fff":
             count = count + 1
+
     print("汉字 in total: ", count)
     print("汉字+标点 in total: ", count + num_of_punc)
+
     return text
 
 
@@ -139,29 +174,35 @@ def show_repeat(in_list, r_type, k=3):
     # Set up value >= 3 cuz only when the times of appearance is >= 3, we count it as repetition
     output_dict = {key: value for key, value in b.items() if value >= k}
     if len(output_dict):
-        print("Repetition of",r_type, "type detected:")
+        print("Repetition of", r_type, "type detected:")
         for key, value in output_dict.items():
-            print(key, ": ", value, "times!") # show the repeated elements and the correspinding times
+            print(
+                key, ": ", value, "times!"
+            )  # show the repeated elements and the correspinding times
     else:
         print("This text doesn't contain any", r_type, "repetition!")
 
-    
-def main():
 
+def main():
     input_chinese = read_file(os.path.join(RELA_PATH, FILE_NAME))
     cleaned_text = clean_text(input_chinese)
     tokenized_text = seg_char(cleaned_text)
-    # short but a lot repetition``
+    # short but a lot repetition
     len_repe_short_but_many = 4  # Minimum length to be counted as repetition, default=3
-    times_repe_short_but_many = 3  # Minimum times of occurrence to be counted as repetition, default=3
+    times_repe_short_but_many = (
+        3  # Minimum times of occurrence to be counted as repetition, default=3
+    )
     three_grams = ngram_analysis(tokenized_text, len_repe_short_but_many)
     show_repeat(three_grams, "short", times_repe_short_but_many)
 
     # few but long repetition
-    len_repe_few_but_long= 8  # Minimum length to be counted as repetition, default=3
-    times_repe_few_but_long = 2  # Minimum times of occurrence to be counted as repetition, default=3
+    len_repe_few_but_long = 8  # Minimum length to be counted as repetition, default=3
+    times_repe_few_but_long = (
+        2  # Minimum times of occurrence to be counted as repetition, default=3
+    )
     three_grams = ngram_analysis(tokenized_text, len_repe_few_but_long)
     show_repeat(three_grams, "long", times_repe_few_but_long)
+
 
 if __name__ == "__main__":
     main()
